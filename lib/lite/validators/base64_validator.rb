b3c0db5a5af7aad13e2bc01e6a986ad2c78ff2c5
Juan Gomez <j.gomez@drexed.com>
2019-06-26 12:07:17 -0400

    Linter fixes

--- lib/lite/validators/base64_validator.rb
+++ lib/lite/validators/base64_validator.rb
@@ -2,6 +2,8 @@
 
 class Base64Validator < BaseValidator
 
+  # rubocop:disable Metrics/LineLength
   REGEXP ||= /^(?:[A-Za-z0-9+$]{4})*(?:[A-Za-z0-9+$]{2}==|[A-Za-z0-9+$]{3}=|[A-Za-z0-9+$]{4})$/.freeze
+  # rubocop:enable Metrics/LineLength
 
 end
