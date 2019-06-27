1c2028f095f27c3d48bd86cd7e3ffa128ad7704e
Juan Gomez <j.gomez@drexed.com>
2019-06-27 11:54:49 -0400

    Add isin validator

--- lib/lite/validators.rb
+++ lib/lite/validators.rb
@@ -10,7 +10,7 @@ require 'active_model'
 %w[
   version base_validator alpha_validator alpha_numeric_validator base64_validator boolean_validator
   coordinate_validator credit_card_validator currency_validator cusip_validator email_validator
-  equality_validator hex_validator imei_validator ip_address_validator isbn_validator
+  equality_validator hex_validator imei_validator ip_address_validator isbn_validator isin_validator
 ].each do |name|
   require "lite/validators/#{name}"
 end
---
+++ spec/lite/isin_validator_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe IsinValidator do
+  before do
+    class IsinKlass < MockedKlass
+
+      validates :input_0, isin: true
+
+    end
+  end
+
+  let(:klass) { IsinKlass.new }
+
+  describe '#validate' do
+    it 'to be valid' do
+      pass!('AU0000XVGZA3')
+      pass!('US0378331005')
+    end
+
+    it 'to not be valid' do
+      fail!('US03783310055')
+      fail!('US037833100')
+      fail!('US03783315')
+      fail!('AA0000XVGZA3')
+      fail!('120378331004')
+    end
+  end
+
+end
