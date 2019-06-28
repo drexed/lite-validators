57cd0d6294661b97a05b1e52d5641d692a476862
Juan Gomez <j.gomez@drexed.com>
2019-06-28 17:55:50 -0400

    Clean up linter

--- lib/lite/validators.rb
+++ lib/lite/validators.rb
@@ -10,10 +10,10 @@ require 'active_model'
 %w[
   version base_validator alpha_validator alpha_numeric_validator base64_validator boolean_validator
   coordinate_validator credit_card_validator currency_validator cusip_validator email_validator
-  equality_validator file_size_validator hex_validator imei_validator ip_address_validator isbn_validator isin_validator
-  mac_address_validator name_validator password_validator phone_number_validator sedol_validator
-  slug_validator ssn_validator time_zone_validator type_validator url_validator username_validator
-  uuid_validator
+  equality_validator file_size_validator hex_validator imei_validator ip_address_validator
+  isbn_validator isin_validator mac_address_validator name_validator password_validator
+  phone_number_validator sedol_validator slug_validator ssn_validator time_zone_validator
+  type_validator url_validator username_validator uuid_validator
 ].each do |name|
   require "lite/validators/#{name}"
 end
